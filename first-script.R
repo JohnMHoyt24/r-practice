e260105adc3e82b9136d4a4d9dc3efb84063ba0a
JohnMHoyt24 <67488201+JohnMHoyt24@users.noreply.github.com>
2020-12-08 21:58:08 -0500

    My first R script

---
+++ first-script.R
@@ -0,0 +1,41 @@
+library(tidyverse)
+#First fake data will be generated
+sales <- data.frame(
+                  neighborhood = c("Peaceful Glen", "Peaceful Glen", "Peaceful Glen",
+                    "Peaceful Glen", "Pleasant Valley", "Pleasant Valley",
+                    "Pleasant Valley", "Pleasant Valley", "Pleasant Valley"),
+                  property_class = c("Dwelling", "Dwelling", "Mobile Home",
+                                     "Mobile Home", "Commercial", "Commercial",
+                                     "Dwelling", "Dwelling", "Mobile Home"),
+                  sales_price = c(22900, 349900, 235000, 29000, 340000, 940250,
+                                  510000, 429382, 9000)
+                  )
+head(x = sales, n = 5) #Displays the first five rows of data
+
+total_sales <- nrow(sales) #Displays the number of rows of data
+
+dwelling_sales <- filter(sales,
+                         property_class == "Dwelling") #Keep just Dwelling sales
+
+head(x = dwelling_sales, n = 5)
+
+#Average price by neighborhood
+dwelling_sales %>%
+  group_by(neighborhood) %>%
+  summarize(
+    n_sales = n(),
+    mean_price = mean(sales_price),
+    median_price = median(sales_price)
+  ) %>%
+  arrange(median_price)
+
+#plot all sales prices by neighborhood
+dwelling_sales %>%
+  ggplot(aes(sales_price)) +
+  geom_histogram() +
+  scale_x_log10(labels = scales::label_dollar()) +
+  facet_wrap(~ neighborhood, scale = "free_y") +
+  labs(
+    x = "Sales price",
+    y = "Number of sales"
+  )
